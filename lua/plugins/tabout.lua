4a9187313bf04f2bf064055181fab5d8f8b240fa
pickleginderick <ginderickgmail.com>
2024-02-26 20:30:20 +0800

    replace tabout to neotab

--- lua/plugins/tabout.lua
+++ lua/plugins/tabout.lua
@@ -1,26 +1,20 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    dependencies = { "abecodes/tabout.nvim" },
+    build = "make install_jsregexp",
+    dependencies = { "neotab.nvim", },
     keys = {
       {
-        "<tab>",
+        "<Tab>",
         function()
-          return "<Plug>(Tabout)" or (require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next") or "<tab>"
+          return require("luasnip").jumpable(1)       --
+              and "<Plug>luasnip-jump-next"
+              or "<Plug>(neotab-out)"
         end,
         expr = true,
         silent = true,
         mode = "i",
       },
     },
-  },
-  {
-    "abecodes/tabout.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "hrsh7th/nvim-cmp",
-    },
-    event = "InsertEnter",
-    config = true,
-  },
+  }
 }
