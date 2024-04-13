3d9af7cda991450dd975fabf19af31946fed95d6
Gin Derick Magno <gin.magno@globe.com.ph>
2024-04-13 08:40:39 +0800

    update configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.swapfile = false
-vim.opt.relativenumber = false
-vim.opt.timeoutlen = 1000
-vim.opt.ttimeoutlen = 0
+vim.opt.relativenumber = true
 
 -- Gitsigns
 opts = {
