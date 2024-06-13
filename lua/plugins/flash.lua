709360ed3a2e09c483c8548920873ca2e0c1b160
Gin Derick Magno <gin.magno@globe.com.ph>
2024-06-13 09:04:01 +0800

    set flash.nvim to true

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,32 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  vscode = true,
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      -- options used when flash is activated through
+      -- a regular search with `/` or `?`
+      search = {
+        -- when `true`, flash will be activated during regular search by default.
+        -- You can always toggle when searching with `require("flash").toggle()`
+        enabled = true,
+        highlight = { backdrop = false },
+        jump = { history = true, register = true, nohlsearch = true },
+        search = {
+          -- `forward` will be automatically set to the search direction
+          -- `mode` is always set to `search`
+          -- `incremental` is set to `true` when `incsearch` is enabled
+        },
+      },
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
